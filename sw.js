39e503f9a3cd9cf6378d8a77618e1003461791bd
Codeship <jakob.viskum.damgaard@gmail.dk>
2017-07-22 11:02:37 +0000

    Updates

--- sw.js
+++ sw.js
@@ -19,79 +19,80 @@ importScripts('workbox-sw.prod.v1.1.0.js');
  */
 const fileManifest = [
   {
-    "url": "0.nuxt.bundle.94ac484113cae905114a.js",
-    "revision": "536cc02ada00355e4d599e5a2a7f181f"
+    "url": "/_nuxt/0.nuxt.bundle.54d2c58c6f37644978b7.js",
+    "revision": "b0970a71b58a9ba68ccdd07cd37c547c"
   },
   {
-    "url": "1.nuxt.bundle.1e30ee63b06499fc67a3.js",
-    "revision": "ac539d0339aeaecd491ae6f8c237d625"
+    "url": "/_nuxt/1.nuxt.bundle.ddb7a1d62327f8f99f72.js",
+    "revision": "052f94525c2b08610452810ca5554d04"
   },
   {
-    "url": "2.nuxt.bundle.edac010722ebad2435ca.js",
-    "revision": "b131fcedaed50fe124afa43e1e0a03e6"
+    "url": "/_nuxt/2.nuxt.bundle.c9b78a799590aff14f94.js",
+    "revision": "60f6abe6b64d35a6e35c37f803a4a5f8"
   },
   {
-    "url": "3.nuxt.bundle.0f35dbd4c331c8fb3eb7.js",
-    "revision": "1950fdaae6c526055576cca80d341edd"
+    "url": "/_nuxt/3.nuxt.bundle.4de9734dd13c6290e027.js",
+    "revision": "0b82bb3d707924dbc1d781cddadfad18"
   },
   {
-    "url": "4.nuxt.bundle.0196306f6aa8a8478add.js",
-    "revision": "b5ab6d7bcdf18a4ec9af49086c5f8660"
+    "url": "/_nuxt/4.nuxt.bundle.0349425f3e44986fd372.js",
+    "revision": "2c25755753c7d72c5ab20dacb70c48a4"
   },
   {
-    "url": "5.nuxt.bundle.555085a4681f2d3d1c8c.js",
-    "revision": "019420f0b9a2b46764d53acddc4499db"
+    "url": "/_nuxt/5.nuxt.bundle.3a39aa19bd2310e42da6.js",
+    "revision": "fd9565601e86ffc18a397653c939aa1a"
   },
   {
-    "url": "6.nuxt.bundle.3153b376e5e8e461655d.js",
-    "revision": "b8527ca566ac312cd62f522709086cd3"
+    "url": "/_nuxt/6.nuxt.bundle.9e08afad35dd39d44d6d.js",
+    "revision": "6eeca297534f4cf8d7ef2c8640f5359c"
   },
   {
-    "url": "7.nuxt.bundle.a8f5241e283c9250133c.js",
-    "revision": "64da99e2e0f12762bfa94889dc60428e"
+    "url": "/_nuxt/7.nuxt.bundle.902f296b9476b153750a.js",
+    "revision": "7c1f14e00682bf5939a261f5421c936e"
   },
   {
-    "url": "8.nuxt.bundle.35664468ca1a455d4f38.js",
+    "url": "/_nuxt/8.nuxt.bundle.35664468ca1a455d4f38.js",
     "revision": "21ceab6c92bdba147f0233104610a618"
   },
   {
-    "url": "9.nuxt.bundle.e54abb2ce01e82ac8e2d.js",
+    "url": "/_nuxt/9.nuxt.bundle.e54abb2ce01e82ac8e2d.js",
     "revision": "a034e3c809b2210b24050ff78c8de7c6"
   },
   {
-    "url": "client-manifest.json",
-    "revision": "f92eaf36987c4269fd1e58a24c512863"
+    "url": "/_nuxt/client-manifest.json",
+    "revision": "ad240db9adee2939e865c94c86b76489"
   },
   {
-    "url": "index.html",
+    "url": "/_nuxt/index.html",
     "revision": "744fb1c184ce0e85af057191b683d6a2"
   },
   {
-    "url": "manifest.32a5e7a8.json",
+    "url": "/_nuxt/manifest.32a5e7a8.json",
     "revision": "a8c86be0acce4fc1d76ef686ebc9ebf7"
   },
   {
-    "url": "manifest.660829e5ca41ae1cef2e.js",
-    "revision": "f8e5f1bdafa0cf33694490495b27a02f"
+    "url": "/_nuxt/manifest.cfb93e6f4945a1bd0941.js",
+    "revision": "d9bcd2488798e682fef75e9407f74239"
   },
   {
-    "url": "nuxt.bundle.ccce89666b57f2def26a.js",
-    "revision": "f0f27cc6ca5445c47cfcae2f5e9b9598"
+    "url": "/_nuxt/nuxt.bundle.6f30d40fa51286f80ddb.js",
+    "revision": "6fee2d2c86aa2806909f928852d7f99d"
   },
   {
-    "url": "server-bundle.json",
-    "revision": "4cc1cef144ff60a619241fe56a50cffc"
+    "url": "/_nuxt/server-bundle.json",
+    "revision": "7900210e21b207bf7df28bf10e3e6f64"
   },
   {
-    "url": "vendor.bundle.c8420611220caea7ca12.js",
+    "url": "/_nuxt/vendor.bundle.c8420611220caea7ca12.js",
     "revision": "e7fc6707e50afa8a3b918e6e0f825bc3"
   }
 ];
 
 const workboxSW = new self.WorkboxSW({
   "cacheId": "www_0.1.0",
+  "clientsClaim": true,
   "directoryIndex": "/"
 });
 workboxSW.precache(fileManifest);
-workboxSW.router.registerRoute('/\/_nuxt\/.*/', workboxSW.strategies.cacheFirst({}), 'GET');
-workboxSW.router.registerRoute('/\/.*/', workboxSW.strategies.networkFirst({}), 'GET');
+workboxSW.router.registerRoute('/_nuxt/**', workboxSW.strategies.cacheFirst({}), 'GET');
+workboxSW.router.registerRoute('/**', workboxSW.strategies.networkFirst({}), 'GET');
