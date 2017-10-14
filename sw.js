144424c13c61eea84a42c68ed64d2b38dd31de76
Codeship <jakob.viskum.damgaard@gmail.dk>
2017-10-14 19:06:46 +0000

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts('workbox-sw.prod.v1.1.0.js');
+importScripts('workbox-sw.prod.v1.3.0.js');
 
 /**
  * DO NOT EDIT THE FILE MANIFEST ENTRY
@@ -19,72 +19,52 @@ importScripts('workbox-sw.prod.v1.1.0.js');
  */
 const fileManifest = [
   {
-    "url": "/_nuxt/0.nuxt.bundle.6527753982b5e0654618.js",
-    "revision": "fe829b66b9de91f4f453705be658cfc4"
+    "url": "/_nuxt/app.2a434b288503c9cbfd3b.js",
+    "revision": "7df2ff484c9413816389d3e3c909e536"
   },
   {
-    "url": "/_nuxt/1.nuxt.bundle.d7a1f10818f0eba913cb.js",
-    "revision": "f2d97d3adc912c490d6e2a98fb77a38c"
+    "url": "/_nuxt/common.51cab404e03a0f40520f.js",
+    "revision": "0bf182d9e43cdd60fd3a4003ee08802c"
   },
   {
-    "url": "/_nuxt/2.nuxt.bundle.dbcee5bbf8db6f15aced.js",
-    "revision": "d046422321e03ded986c0d91d7258383"
+    "url": "/_nuxt/layouts/default.40d074fdb0f45596de26.js",
+    "revision": "15b0f2e50a8d2c0d97433bc4e6e16567"
   },
   {
-    "url": "/_nuxt/3.nuxt.bundle.ac0907e214cd3ef5a3e3.js",
-    "revision": "12f07c273e02b0c90eee4d9cc9ad901b"
+    "url": "/_nuxt/manifest.7374d4ed8a8e04da0073.js",
+    "revision": "036fcb6d0b9658e770c56c0ed01488b2"
   },
   {
-    "url": "/_nuxt/4.nuxt.bundle.cc6dae11d63616441c5b.js",
-    "revision": "a541d457a3cd021047835569dc334718"
+    "url": "/_nuxt/pages/_page/index.b5c57f765a2b1b570e36.js",
+    "revision": "a75a35fa1bc515983cb767105583939c"
   },
   {
-    "url": "/_nuxt/5.nuxt.bundle.c74e5e4dc59c34f30b7e.js",
-    "revision": "f99553081ed2ccefbc8b3a2e526d448d"
+    "url": "/_nuxt/pages/blog/_blog.3fdafc96f64dca9ba197.js",
+    "revision": "cc2e25850c45adcb7ab48bb750ffd6aa"
   },
   {
-    "url": "/_nuxt/6.nuxt.bundle.85478a4551a1efbbb27f.js",
-    "revision": "034969cd4e8090967b05a871abb56ac9"
+    "url": "/_nuxt/pages/blog/index.c668f8f0cbc3b06f5a9e.js",
+    "revision": "8680c43a9733bf0ee9b985c516e9a7e4"
   },
   {
-    "url": "/_nuxt/7.nuxt.bundle.2188bfe48608b8454405.js",
-    "revision": "b6b99b34b84841a49058eaed0e0597d7"
+    "url": "/_nuxt/pages/events/_event.2f520362f8aabfa21016.js",
+    "revision": "f4d587149bd37255a9573d4d89ed359f"
   },
   {
-    "url": "/_nuxt/8.nuxt.bundle.35664468ca1a455d4f38.js",
-    "revision": "21ceab6c92bdba147f0233104610a618"
+    "url": "/_nuxt/pages/events/index.85b2197e0a1cf12c9b6b.js",
+    "revision": "b62e043ba380ab0d59a00088ba25e457"
   },
   {
-    "url": "/_nuxt/9.nuxt.bundle.e54abb2ce01e82ac8e2d.js",
-    "revision": "a034e3c809b2210b24050ff78c8de7c6"
+    "url": "/_nuxt/pages/index.f1c7113d691abc52af11.js",
+    "revision": "6222a8f36ca7ed155313a6d95a86bf42"
   },
   {
-    "url": "/_nuxt/client-manifest.json",
-    "revision": "0f8c2d16138a7f2830ef151412c9f549"
+    "url": "/_nuxt/pages/vi-tilbyder/_service.5904d2b1ca9fd5d62e35.js",
+    "revision": "eb0807425fb6b2f193051a2d38b60612"
   },
   {
-    "url": "/_nuxt/index.html",
-    "revision": "744fb1c184ce0e85af057191b683d6a2"
-  },
-  {
-    "url": "/_nuxt/manifest.32a5e7a8.json",
-    "revision": "a8c86be0acce4fc1d76ef686ebc9ebf7"
-  },
-  {
-    "url": "/_nuxt/manifest.ca3ce14c44e2975ed3ee.js",
-    "revision": "8f51fad603f941be221ecb341c51ac3a"
-  },
-  {
-    "url": "/_nuxt/nuxt.bundle.6f30d40fa51286f80ddb.js",
-    "revision": "6fee2d2c86aa2806909f928852d7f99d"
-  },
-  {
-    "url": "/_nuxt/server-bundle.json",
-    "revision": "4527d16edd2d64c14372d2b89c80cdb8"
-  },
-  {
-    "url": "/_nuxt/vendor.bundle.c8420611220caea7ca12.js",
-    "revision": "e7fc6707e50afa8a3b918e6e0f825bc3"
+    "url": "/_nuxt/pages/vi-tilbyder/index.2c653f8101a2ec3d89d3.js",
+    "revision": "afed1f3db2d70ba88756af6103c659fd"
   }
 ];
 
@@ -94,5 +74,5 @@ const workboxSW = new self.WorkboxSW({
   "directoryIndex": "/"
 });
 workboxSW.precache(fileManifest);
-workboxSW.router.registerRoute('/_nuxt/**', workboxSW.strategies.cacheFirst({}), 'GET');
 workboxSW.router.registerRoute('/**', workboxSW.strategies.networkFirst({}), 'GET');
+workboxSW.router.registerRoute('/_nuxt/**', workboxSW.strategies.cacheFirst({}), 'GET');
